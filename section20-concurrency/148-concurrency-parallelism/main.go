089df4849fe8bce4ad89d5a85373477c8440162a
Baby Lin <58794134+HongbinLin1995@users.noreply.github.com>
2022-03-31 14:26:09 -0700

    Update main.go

--- section20-concurrency/148-concurrency-parallelism/main.go
+++ section20-concurrency/148-concurrency-parallelism/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	go foo()
 	bar()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 }
 
 func foo() {
